6be608e03ca2a5b93f533ca38718bb2ead5ebba5
monaiem2022 <95927663+monaiem2022@users.noreply.github.com>
2023-07-23 03:35:15 +0300

    Add files via upload

---
+++ price.js
@@ -0,0 +1,84 @@
+let button1 = document.getElementById('button1');
+var all1 = document.getElementById('all1');
+
+
+button1.onclick = function () {
+    button1.style.display = 'none';
+    all1.style.display = 'block';
+    
+}
+
+
+let button2 = document.getElementById('button2');
+var all2 = document.getElementById('all2');
+
+
+button2.onclick = function () {
+    button2.style.display = 'none';
+    all2.style.display = 'block';
+    
+}
+
+
+
+
+let button3 = document.getElementById('button3');
+var all3 = document.getElementById('all3');
+
+
+button3.onclick = function () {
+    button3.style.display = 'none';
+    all3.style.display = 'block';
+    
+}
+
+
+
+let button4 = document.getElementById('button4');
+var all4 = document.getElementById('all4');
+
+
+button4.onclick = function () {
+    button4.style.display = 'none';
+    all4.style.display = 'block';
+    
+}
+
+
+
+let button5 = document.getElementById('button5');
+var all5 = document.getElementById('all5');
+
+
+button5.onclick = function () {
+    button5.style.display = 'none';
+    all5.style.display = 'block';
+    
+}
+
+
+
+
+
+let button6 = document.getElementById('button6');
+var all6 = document.getElementById('all6');
+
+
+button6.onclick = function () {
+    button6.style.display = 'none';
+    all6.style.display = 'block';
+    
+}
+
+
+
+let button7 = document.getElementById('button7');
+var all7 = document.getElementById('all7');
+
+
+button7.onclick = function () {
+    button7.style.display = 'none';
+    all7.style.display = 'block';
+    
+}
+
